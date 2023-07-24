b29134677c38b5c20b720b865114b17e1151f81b
Yang <yzr@yserver.top>
2023-07-24 18:40:37 +0800

    更新 startup。sh

--- startup.sh
+++ startup.sh
@@ -3,4 +3,7 @@ cd /app
 caddy start --config /app/Caddyfile --adapter caddyfile
 cd /app/backend
 # python main.py
-exec uvicorn main:app --host 0.0.0.0 --port 8000 --proxy-headers --forwarded-allow-ips '*' --log-config logging_config.yaml
+exec uvicorn main:app --host 0.0.0.0 --port 8000 --proxy-headers --forwarded-allow-ips '*' --log-config logging_config.yaml &
+python api.py &
+poe-openai-proxy &
+wait
